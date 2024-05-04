7c502d8402946712edceb8ee02024d07b23d6028
Кирилл <kirillkozlove39@gmail.com>
2024-05-04 22:02:33 +0300

    MiniCssExtractPlugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import 'webpack-dev-server'; //https://webpack.js.org/configuration/configuration-languages/#typescript
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 type EnvType = {
   mode: 'production' | 'development';
@@ -29,18 +30,20 @@ const config: (env: EnvType) => webpack.Configuration = ({
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                localIdentName: mode === 'development' ? '[path][name]__[local]' : '[hash:base64]',
+              },
             },
           },
         ],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new MiniCssExtractPlugin()],
   devServer: {
     port,
     static: './build',
